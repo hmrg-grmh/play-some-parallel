711142f1cb6b077c8ae470f31479ab7f4736c95d
Hm Renga Y <68635334+hmrg-grmh@users.noreply.github.com>
2021-04-05 21:56:04 +0800

    Update ParTest.scala
    (1,11)
    (3,13)
    (2,12)
    (4,14)
    ----------
    (2,12)
    (4,14)
    (3,13)
    (1,11)
    ----------
    (3,3)
    (2,2)
    (4,4)
    (1,1)

--- on_Scala/play/ParTest.scala
+++ on_Scala/play/ParTest.scala
@@ -2,12 +2,21 @@ package play
 
 object ParTest extends App
 {
-    (1 to 9).toList.zip(11 to 19)
+    (1 to 4).toList.zip(11 to 14)
             .par
             .foreach(println)
     
     println("----------")
     
-    ((1 to 9).toList.par zip (11 to 19).toList.par)
+    ((1 to 4).toList.par zip (11 to 14).toList.par)
         .foreach(println)
+    
+    
+    println("----------")
+    
+    
+    val x : scala.collection.parallel.immutable.ParSeq[Int] = (1 to 4).toList.par
+    val xx : scala.collection.parallel.immutable.ParRange = (1 to 4).par
+    
+    (xx zip xx) foreach println
 }
